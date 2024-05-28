7424a91092402609ad23f8302a5abf67eed1d56d
Elliott Indiran <eindiran@users.noreply.github.com>
2024-05-28 02:25:04 -0400

    Major script improvements

--- countdown/arithmetic.py
+++ countdown/arithmetic.py
@@ -7,6 +7,7 @@ Solver for the arithmetic puzzles on Countdown.
 """
 
 import itertools
+import random
 from typing import Union
 
 NullableInt = Union[int, None]
@@ -86,12 +87,38 @@ def solve_cdarithmetic(target: int, inputs):
     return ""
 
 
+def loop_mode(loops: int):
+    """
+    Launch into looping over runs.
+    """
+    for _ in range(loops):
+        target = random.randint(101, 999)
+        inputs = [
+            random.choice([25, 50, 75, 100]),
+            random.choice([25, 50, 75, 100]),
+            random.randint(1, 10),
+            random.randint(1, 10),
+            random.randint(1, 10),
+            random.randint(1, 10),
+        ]
+        print(f"Target: {target}")
+        print(f"Inputs: {inputs}")
+        res = solve_cdarithmetic(target, inputs)
+        print(f"Result: {res if res else 'no solution found'}\n\n")
+
+
 if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser(description="Solve Countdown arithmetic puzzles from the CLI")
-    parser.add_argument("target", type=int, help="Target integer")
-    parser.add_argument("inputs", type=int, nargs="+", help="Input integers")
+    subparsers = parser.add_subparsers()
+    solve = subparsers.add_parser("solve", help="Command to run a single solution")
+    solve.add_argument("target", type=int, help="Target integer")
+    solve.add_argument("inputs", type=int, nargs="+", help="Input integers")
+    loop = subparsers.add_parser("loop", help="Command to loop over random inputs")
+    loop.add_argument("loops", type=int, help="Iniate looping n times over random runs")
     args = parser.parse_args()
-
-    print(solve_cdarithmetic(args.target, args.inputs))
+    if vars(args).get("loops"):
+        loop_mode(args.loops)
+    else:
+        print(solve_cdarithmetic(args.target, args.inputs))
